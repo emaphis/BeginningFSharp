f2379343e7e9c255eb1931295d3eaf70ec7438a7
emaphis <emaphis85@gmail.com>
2021-11-19 20:59:57 -0500

    using bindings

---
+++ BegFSharp/Chap03/UseBindings.fs
@@ -0,0 +1,12 @@
+module UseBindings
+
+open System.IO
+
+// function to read first line from a file
+let readFirstLine filename =
+    // open file using a "use" binding
+    use file = File.OpenText filename
+    file.ReadLine()
+
+// call function and print the result
+printfn "First line was: %s" (readFirstLine "Chap03\\mytext.txt")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BeginningFSharp
+
 Beginning F# 4.0 by Pickering and Eason
+
+Another attempt at learning F#
