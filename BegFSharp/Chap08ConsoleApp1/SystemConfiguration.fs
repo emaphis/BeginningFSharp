c8bed4326b1319598ac6388220ef0a8301185523
emaphis <emaphis85@gmail.com>
2021-11-28 15:30:50 -0500

    The System.Configuration Namespace

---
+++ BegFSharp/Chap08ConsoleApp1/SystemConfiguration.fs
@@ -0,0 +1,41 @@
+﻿namespace SystemConfiguration
+// The System.Configuration Namespace  - pg. 167
+
+open System
+
+// This will only work in a compiled context, not in F# Interactive.
+
+module Demo =
+
+    open System.Configuration
+
+
+    // read an application setting
+    let setting = ConfigurationManager.AppSettings.["MySetting"]
+    
+    // print the setting 
+    printfn "My Setting: %s\n" setting
+
+    // get the connection string
+    let connectionStringDetails =
+        ConfigurationManager.ConnectionStrings.["MyConnectionString"]
+
+    // print the details 
+    printfn "Connection details:\n%s\r\n%s\n"
+        connectionStringDetails.ConnectionString
+        connectionStringDetails.ProviderName
+
+    // open the machine config
+    let config = 
+        ConfigurationManager.OpenMachineConfiguration()
+
+    // print the names of all sections
+    printfn "Machine Config:"
+    for x in config.Sections do
+        printfn "%s" x.SectionInformation.Name
+
+    [<EntryPoint>]
+    let main args =
+        printfn "Press a key to exit."
+        Console.ReadKey(false) |> ignore
+        0
