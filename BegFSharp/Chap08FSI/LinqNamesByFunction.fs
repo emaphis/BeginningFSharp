26814e59bd305bf47703afaf7ddacc0e0ad0698c
emaphis <emaphis85@gmail.com>
2021-11-29 16:07:07 -0500

    Introducing LINQ

---
+++ BegFSharp/Chap08FSI/LinqNamesByFunction.fs
@@ -0,0 +1,22 @@
+﻿// Introducing LINQ  - pg. 190
+
+module Strangelights.LinqNamesByFunction
+
+open Strangelights.LinqImports
+
+
+/// query string methods using functions
+let namesByFunction =
+    (typeof<string>).GetMethods()
+    |> where (fun m -> not m.IsStatic)
+    |> groupBy (fun m -> m.Name)
+    |> select (fun m -> m.Key, count m)
+    |> orderBy (fun (_, m) -> m)
+
+
+let printFunctions() =
+    namesByFunction
+    |> Seq.iter (fun (name, count) -> printf "%s - %i\n" name count)
+
+
+//printFunctions()    
--- BegFSharp/Chap08FSI/Program.fs
+++ BegFSharp/Chap08FSI/Program.fs
@@ -1,11 +1,14 @@
 ﻿// Main App
 
 open System
-open SystemXML
+//open SystemXML
+open Strangelights.LinqNamesByFunction
+
 
 [<EntryPoint>]
 let main args =
     printfn "Hello from F#"
-    printFruits()
+    //printFruits()
+    printFunctions()
     Console.ReadKey(false) |> ignore
     0
