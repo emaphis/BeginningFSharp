d3a76669146d44369211dbc9169513ec910a3af5
emaphis <emaphis85@gmail.com>
2021-11-28 19:04:53 -0500

    The System.IO Namespace

---
+++ BegFSharp/Chap08ConsoleApp2/Program.fs
@@ -0,0 +1,10 @@
+﻿// Main App
+
+open System
+
+
+[<EntryPoint>]
+let main args =
+    printfn "Hello from F#"
+    Console.ReadKey(false) |> ignore
+    0
---
+++ BegFSharp/Chap08FSI/SystemIO.fs
@@ -0,0 +1,42 @@
+﻿//  The System.IO Namespace  - pg. 170
+
+module SystemIO
+
+open System.IO
+
+// list all the files in the root of the C: drive
+let files = Directory.GetFiles(@"c:\")
+
+// Write out various information about the file
+for filepath in files do
+    let file = new FileInfo(filepath)
+    printfn "%s\t%d\t%O"
+        file.Name
+        file.Length
+        file.CreationTime
+
+
+let dir = Directory.GetCurrentDirectory()
+
+open System.IO
+
+//test.csv: 
+//Apples,12,25 
+//Oranges,12,25 
+//Bananas,12,25 
+
+// open a test file and print the contents
+let readFile() =
+    let lines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "/test.csv") 
+    
+    let printLine (line: string) =
+        let items = line.Split([| ',' |])
+        printfn "%O %O %O"
+            items[0]
+            items[1]
+            items[2]
+
+    Seq.iter printLine lines
+
+
+readFile()
