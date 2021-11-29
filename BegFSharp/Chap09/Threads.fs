5aeadc1a443a4e7733e2f9abd7a6ec15211dd2f8
emaphis <emaphis85@gmail.com>
2021-11-29 17:10:21 -0500

    Start on chapter 9 - Parallel Programming

---
+++ BegFSharp/Chap09/Threads.fs
@@ -0,0 +1,26 @@
+﻿// Threads, Memory, Locking, and Blocking  - pg. 198
+
+module Threads
+
+open System.Threading
+
+
+/// explicit threading
+let main() =
+    // Create a new thread passing it a lambda function:
+    let thread = new Thread( fun () ->
+        // Print a message on the newly created thread
+        printfn "Created thread: %i" Thread.CurrentThread.ManagedThreadId)
+
+    // Start the new thread
+    thread.Start()
+
+    // Print a message onthe original thread
+    printfn "Original thread: %i" Thread.CurrentThread.ManagedThreadId
+
+    // Wait for the created in exit:
+    thread.Join()
+
+
+do main()
+
