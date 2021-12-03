c40f8bb3a9e81e6bcb930c330a80232c9715eaf0
emaphis <emaphis85@gmail.com>
2021-12-03 15:49:59 -0500

    Lesson14 - Testing operations, creating audit functions

--- GetProgramming/Lesson14-Capstone2/Capstone2/Scratchpad1.fsx
+++ GetProgramming/Lesson14-Capstone2/Capstone2/Scratchpad1.fsx
@@ -40,7 +40,7 @@ let id_1 = account1.AccountID
 /// Deposits an amount into an account
 //let deposit (amount: decimal) (account: Account) : Account =
 //    { AccountID = Guid.Empty; Owner = { Name = "Sam" }; Balance = 10M }
-    
+
 
 /// Deposits an amount into an account
 let deposit amount account =
@@ -57,7 +57,7 @@ let withdraw amount account =
 
 
 
-// examples
+// operations examples
 let customer2 = { Name = "Matt" }
 
 let account2 =
@@ -66,6 +66,105 @@ let account2 =
     Balance = 100M }
 
 
-let balance2 =   (account2 |> deposit 50M |> withdraw 25 |> deposit 10M).Balance
-printf "Balance should be 135M: %A" balance2
+let account2a =   (account2 |> deposit 50M |> withdraw 25 |> deposit 10M)
+printfn "Balance should be 135M: %A" account2a.Balance
 
+// over withdraw should return old account
+let balance2a = (withdraw 150M account2a).Balance
+printfn "Balance should be 135M: %A" balance2a
+
+
+
+/////////////////////////////////////////////////
+// 14.6 Abstraction and reuse through higher-order functions - pg. 163
+
+// Listing 14.3 Creating pluggable audit functions
+
+
+open System.IO
+let dir = @"C:\tmp\learnfs\capstone2\"
+
+//let outDir = sprintf "%s%s" dir account1.Owner.Name
+//let filePsth = sprintf "%s\%s.txt" outDir account1.Owner.Name
+
+/// Auditor that writes to filesystem
+let fileSystemAudit account message =
+    let owner = account.Owner.Name
+    let outDir = sprintf "%s%s" dir owner
+    Directory.CreateDirectory(outDir) |> ignore
+    let filePath = sprintf "%s\%s.txt" outDir owner
+    File.AppendAllLines(filePath, [ message ])
+
+
+/// Auditor that prints to console
+let consoleAudit account message =
+    let msg = sprintf "Account %A performed operation '%s', Balance is now $%A " account.AccountID message account.Balance
+    printfn "%s" msg
+
+
+
+// Listing 14.4 Testing functions through scripts  pg. 164
+
+let customer = { Name = "Isacc" }
+let account = { AccountID = Guid.Empty; Owner = customer; Balance = 90M }
+
+// Test out withdraw
+let newAccount = account |> withdraw 10M
+printfn "Should be 80M: %A" newAccount.Balance
+
+// test out console auditor
+consoleAudit account "Testing console audit"
+fileSystemAudit account "Testing file system audit"
+
+
+
+// 14.6.1 Adapting code with higher-order functions
+
+// Listing 14.5 Signature for an orchestration of higher-order function  pg. 165
+
+/// Runs some account operation such as withdraw or deposit with auditing.
+let auditAs (operationName:string) (auditFn:Account -> string -> unit)
+            (operation:decimal -> Account -> Account) (amount:decimal)
+            (account:Account) : Account =
+    let now = DateTime.UtcNow
+    auditFn account (sprintf "%O: Performing a %s operation for £%M..." now operationName amount)
+    let newAccount = operation amount account
+
+    let accountIsUnchanged = (newAccount = account)
+
+    if accountIsUnchanged then auditFn account (sprintf "%O: Transaction rejected!" now) 
+    else auditFn account (sprintf "%O: Transaction accepted! Balance is now £%M." now newAccount.Balance)
+
+    newAccount
+
+
+// testing
+
+// Calling the “raw” deposit and withdraw functions
+let testAcct1 =
+    account
+    |> deposit 100M
+    |> withdraw 50M
+
+printfn "Should be 140M: %A" testAcct1.Balance
+
+
+// Creating new “decorated” versions of deposit and withdraw with console
+// auditing through currying
+let withdrawWithConsoleAudit = auditAs "withdraw" consoleAudit withdraw
+let depositWithConsoleAudit = auditAs "deposit" consoleAudit deposit
+
+let withdrawWithFSAudit = auditAs "withdraw" fileSystemAudit withdraw
+let depositWithFSAudit = auditAs "deposit" fileSystemAudit deposit
+
+
+// Calling the “decorated” versions of deposit and withdraw
+account
+|> depositWithConsoleAudit 100M
+|> withdrawWithConsoleAudit 50M
+
+
+// File System
+//account
+//|> depositWithFSAudit 100M
+//|> withdrawWithFSAudit 50M
